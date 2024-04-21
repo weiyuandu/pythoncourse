230704ddbcd2a42ee009fa48ead63bf9594693c4
Weiyuan Du <145103663+weiyuandu@users.noreply.github.com>
2024-04-22 07:20:07 +0800

    Delete Exercise directory

--- Exercise/L01_Introduction to Python/T01E01-Hello TUM/bonus.py
+++
@@ -1,5 +0,0 @@
-# Jack's name
-jack_name = "Jack"
-
-# Print the sentence with Jack's name
-print(f"Hello TUM! I am {jack_name}.")
--- Exercise/L02_Control Structures/T02E02-Time to Loop/Solution.py
+++
@@ -1,9 +0,0 @@
-# Initialize the sum
-total_sum = 0
-
-# Loop to calculate the sum
-for number in range(1, 101):
-    total_sum += number
-
-# Output the total sum
-print(f"The sum of numbers from 1 to 100 is: {total_sum}")
